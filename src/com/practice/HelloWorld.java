800cc5d54b84f716acb5fc0e0918b4add84c3fb7
Satya Sri <satyasripulipati@gmail.com>
2020-11-07 16:04:09 +0530

    First commit

---
+++ src/com/practice/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(new InputStreamReader(System.in));
+
+        System.out.println("Welcome, May I know your name?");
+        String name = sn.nextLine();
+        System.out.println("Hello, "+name+" !!");
+
+    }
+}
